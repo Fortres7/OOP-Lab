93be8ede97223a2b6c2c6d567bd32ef797749b27
Maarij <127932628+Fortres7@users.noreply.github.com>
2025-04-27 12:08:16 +0500

    Create Q3.cpp

---
+++ lab 9/Q3/Q3.cpp
@@ -0,0 +1,12 @@
+#include "Q3_Book.hpp"
+
+int main()
+{
+    Book book("The C++ Programming Language", "Bjarne Stroustrup", "978-0321563842");
+
+    cout << "Title: " << book.getTitle() << endl;
+    cout << "Author: " << book.getAuthor() << endl;
+    cout << "ISBN: " << book.getISBN() << endl;
+
+    return 0;
+}
